c809537a9a58457cee75e7faede72403a044dbb2
Spanaic <spanaic@gmail.com>
2021-03-03 09:51:54 +0900

    【完了】useEffectのコールバック

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 type props = {
   name: string;
@@ -8,6 +8,29 @@ type props = {
 const App = (props: props) =>  {
   const [state, setState] = useState(props)
   const {name, price} = state
+
+  // useEffectは関数を受け取る
+  // レンダリング後に呼ばれる
+    // componentDidMountに似ている
+    // 値が変わらなければ呼び出されない
+    // 割と頻繁に呼び出されるということを忘れずに！
+  useEffect(() => {
+    console.log('This is like componentDidMount or componetDidUpdate.')
+  })
+
+  // 最初の一回だけ呼び出したい場合
+    // 第二引数に空の配列を渡すだけ！
+    // useEffectはコンポーネント内に幾つでも書ける
+  useEffect(() => {
+    console.log('This is like componentDidMount.')
+  }, [])
+
+  // 特定の状態が変更されたときに呼び出したい場合
+    // 空配列内に変更をwatchする値を入れる
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
+
 
   return (
     <>
