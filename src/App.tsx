ebcf46303986b2da361b3b07569a5bbd89d3788e
Spanaic <spanaic@gmail.com>
2021-03-03 09:23:47 +0900

    【完了】複数の状態管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
-import React, { Fragment, useState } from 'react';
-import { isReturnStatement } from 'typescript';
+import React, { useState } from 'react';
 
-const App = () =>  {
-  // useStateは配列を返す
-    // 状態と関数が返ってくるので分割代入する
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+type props = {
+  name: string;
+  price: number
+}
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
+const App = (props: props) =>  {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const reset = () => setCount(0)
-  const double = () => setCount(previousCount => previousCount * 2)
-  const divide3 = () => setCount(
-    previousCount => {
-      return previousCount % 3 === 0 ? previousCount / 3 : previousCount;
-    }
-    // previousCount => {
-    //   if (previousCount % 3 === 0) {
-    //     return previousCount / 3;
-    //   } else {
-    //     return previousCount
-    //   }
-    // }
-  )
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={divide3}>3の倍数の時岳3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000,
 }
 
 export default App;
