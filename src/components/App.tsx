d005836fd96c5440375178cfe6563aa1a28f8cac
Spanaic <spanaic@gmail.com>
2021-03-21 20:24:07 +0900

    【完了】useReducerを実際に使ってみる

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer, {EventState, EventAction, ActionType} from '../reducers/'
 
-const App = () =>  {
+const App: React.FC = () =>  {
+  // useReducerを使う際の定型文
+  // const [state, dispatch] = useReducer(reducer, initialArg, init)
+  const [state, dispatch] = useReducer<React.Reducer<EventState[], EventAction>>(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  function addEvent(e: React.MouseEvent<HTMLElement, MouseEvent>) {
+    e.preventDefault();
+    console.log({title, body})
+    dispatch({
+      type: ActionType.CREATE_EVENT,
+      payload:
+        {
+          id: 1,
+          title: title,
+          body: body,
+        }
+      }
+    )
+
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form action="">
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title}  onChange={e => setTitle(e.target.value)} />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent} >イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
@@ -37,3 +62,6 @@ const App = () =>  {
 }
 
 export default App;
+function userState(arg0: string): [any, any] {
+  throw new Error('Function not implemented.');
+}
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,25 +1,60 @@
-type actionType = {
-  type: 'CREATE_EVENT' | 'DELETE_EVENT' | 'DELETE_ALL_EVENT';
+// 自分が最初に定義した型(間違い)
+// type actionType = {
+//   // type: 'CREATE_EVENT' | 'DELETE_EVENT' | 'DELETE_ALL_EVENT';
+//   title: string;
+//   body: Text;
+// }
+
+export type EventState = {
+  id: number;
   title: string;
-  body: Text;
+  body: string | number;
+  // body: Text;
+}
+export enum ActionType {
+  CREATE_EVENT = 'CREATE_EVENT',
+  DELETE_EVENT = 'DELETE_EVENT',
+  DELETE_ALL_EVENT = 'DELETE_ALL_EVENT',
+}
+export type EventAction = {
+  type: ActionType;
+  payload: EventState;
 }
 
-// stateは未定義の可能性があるので初期値を入力しておく
-const events = (state = [], action: actionType) => {
+
+const events: React.Reducer<EventState[], EventAction> = (state: EventState[], action: EventAction) => {
   switch(action.type) {
-    case 'CREATE_EVENT':
-      const event = { title: action.title, body: action.body }
+    case ActionType.CREATE_EVENT:
+      const event = { title: action.payload.title, body: action.payload.body }
       const length = state.length
       const id = length === 0 ? 1 : state[length - 1].id + 1
       // 波括弧をスプレッド演算子で展開したときのスコープを考える
       return [...state, { id: id, ...event }]
-    case 'DELETE_EVENT':
+    case ActionType.DELETE_EVENT:
       return state
-    case 'DELETE_ALL_EVENT':
+    case ActionType.DELETE_ALL_EVENT:
       return state
     default:
       return state
   }
 }
+
+// stateは未定義の可能性があるので初期値を入力しておく
+// const events = (state = [], action: actionType) => {
+//   switch(action.type) {
+//     case 'CREATE_EVENT':
+//       const event = { title: action.title, body: action.body }
+//       const length = state.length
+//       const id = length === 0 ? 1 : state[length - 1].id + 1
+//       // 波括弧をスプレッド演算子で展開したときのスコープを考える
+//       return [...state, { id: id, ...event }]
+//     case 'DELETE_EVENT':
+//       return state
+//     case 'DELETE_ALL_EVENT':
+//       return state
+//     default:
+//       return state
+//   }
+// }
 
 export default events
