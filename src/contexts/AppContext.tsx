7c487e5f9ff836908c70ebf38de0b5b77cd09138
Spanaic <spanaic@gmail.com>
2021-03-30 09:59:00 +0900

    【完了】localStrageを用いたデータの永続化

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import eventsReducer, { EventAction, EventStates } from '../reducers/events'
 import operationLogsReducer, { OperationLogAction, OperationLogStates } from '../reducers/operationLogs';
 
@@ -13,20 +13,30 @@ export type OperationLogStateWithAction = {
   operationLogDispatch: React.Dispatch<OperationLogAction>
 }
 
+
 // NOTE: contextを分割して、一つのprovideにまとめる方法を取る
-  // 無理にcombineReducerでreducerをまとめようとしない
-    // https://qiita.com/jonakp/items/58c9c383473d02479ea7
-    // https://qiita.com/jonakp/items/58c9c383473d02479ea7
+// 無理にcombineReducerでreducerをまとめようとしない
+// https://qiita.com/jonakp/items/58c9c383473d02479ea7
+// https://qiita.com/jonakp/items/58c9c383473d02479ea7
 export const EventContext = createContext({} as EventStateWithAction)
 
 export const OperationLogContext = createContext({} as OperationLogStateWithAction)
 
 const EventProvider = (props: { children: React.ReactNode }) => {
-  const initialEventState = {
+  // それぞれのcontextごとにlocalstrageのkeyを設定する
+  const appEventsState = localStorage.getItem('appEvents')
+  // localStrageに保存されていれば、その値を代入することでリロードされてもデータを永続化できる
+  const initialEventState = appEventsState ? JSON.parse(appEventsState) : {
     events: []
   }
 
   const [eventState, eventDispatch] = useReducer<React.Reducer<EventStates, EventAction>>(eventsReducer, initialEventState)
+
+  // NOTE: eventStateをlocalstrageに保存して永続化する
+    // useEffectは第二引数に渡されたstateの変更を感知して、callbackする
+  useEffect(() => {
+    localStorage.setItem('appEvents', JSON.stringify(eventState))
+  }, [eventState])
 
   return (
     <EventContext.Provider value={{ eventState, eventDispatch }}>
@@ -36,11 +46,19 @@ const EventProvider = (props: { children: React.ReactNode }) => {
 }
 
 const OperationLogProvider = (props: { children: React.ReactNode }) => {
-  const initialOperationLogState = {
+  const appLogsState = localStorage.getItem('appLogs')
+  // const initialOperationLogState = appState ? JSON.parse(appState) : {
+    // const initialOperationLogState = {
+  const initialOperationLogState = appLogsState ? JSON.parse(appLogsState) : {
     operationLogs: []
   }
 
   const [operationLogState, operationLogDispatch] = useReducer<React.Reducer<OperationLogStates, OperationLogAction>>(operationLogsReducer, initialOperationLogState)
+
+  // 永続化
+  useEffect(() => {
+    localStorage.setItem('appLogs', JSON.stringify(operationLogState))
+  }, [operationLogState])
 
   return (
     <OperationLogContext.Provider value={{ operationLogState, operationLogDispatch }}>
